490bf3d62c40cbeef7582477f9df8d776ca969c7
christy726 <christygeorge726@gmail.com>
2025-02-27 11:34:53 +0530

    Update generated SMILES file names to reflect new date

--- main.py
+++ main.py
@@ -82,8 +82,8 @@ def main():
     logging.info("Generating new SMILES...")
     new_smiles = [generate_smiles(clm_model, char_to_idx, idx_to_char) for _ in range(5000)]
     logging.info("New SMILES generated")
-    pd.DataFrame(new_smiles, columns=['SMILES']).to_csv('data/generated_smiles_20_02_2025.csv', index=False)
-    logging.info("Generated SMILES saved to 'data/generated_smiles_20_02_2025.csv'")
+    pd.DataFrame(new_smiles, columns=['SMILES']).to_csv('data/generated_smiles_27_02_2025.csv', index=False)
+    logging.info("Generated SMILES saved to 'data/generated_smiles_27_02_2025.csv'")
 
     logging.info("Filtering valid and unique SMILES...")
     valid_new_smiles = []
@@ -95,8 +95,8 @@ def main():
             valid_new_smiles.append(smi)
     logging.info(f"Generated {len(valid_new_smiles)} valid and unique SMILES")
 
-    pd.DataFrame(valid_new_smiles, columns=['SMILES']).to_csv('data/valid_unique_smiles_20_02_2025.csv', index=False)
-    logging.info("Valid and unique SMILES saved to 'data/valid_unique_smiles_20_02_2025.csv'")
+    pd.DataFrame(valid_new_smiles, columns=['SMILES']).to_csv('data/valid_unique_smiles_27_02_2025.csv', index=False)
+    logging.info("Valid and unique SMILES saved to 'data/valid_unique_smiles_27_02_2025.csv'")
 
 if __name__ == "__main__":
     main()
